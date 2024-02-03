51e1092f140b58b14cc83bc5cfaab2d965a44b67
Hansaem Park <49851536+sammitako@users.noreply.github.com>
2024-02-02 21:38:20 -0500

    initial setup

--- app/logs/page.tsx
+++ app/logs/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="text-red-500">Landing Page</div>
+      <div className="">Logs Page</div>
     </main>
   );
 }
