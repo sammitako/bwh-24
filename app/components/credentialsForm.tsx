db47616f17c73d12aebf9ad51ec7cdeceb919486
Aiturgan <aiturgan888@gmail.com>
2024-02-04 08:54:32 -0600

    fixed some bugs with the routing

--- app/components/credentialsForm.tsx
+++ app/components/credentialsForm.tsx
@@ -1,73 +1,59 @@
-"use client";
+'use client';
 
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import Link from 'next/link';
 
 interface CredentialsFormProps {
-  csrfToken?: string;
+	csrfToken?: string;
 }
 
 export function CredentialsForm(props: CredentialsFormProps) {
-  const router = useRouter();
-  const [error, setError] = useState<string | null>(null);
+	const router = useRouter();
+	const [error, setError] = useState<string | null>(null);
 
-  const submit = () => {
-    router.push("/");
-  };
+	const submit = () => {
+		router.push('/');
+	};
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    const data = new FormData(e.currentTarget);
+	const handleSubmit = async (e: any) => {
+		e.preventDefault();
+		const data = new FormData(e.currentTarget);
 
-    const signInResponse = await signIn("credentials", {
-      email: data.get("email"),
-      password: data.get("password"),
-      redirect: false,
-    });
+		const signInResponse = await signIn('credentials', {
+			email: data.get('email'),
+			password: data.get('password'),
+			redirect: false,
+		});
 
-    if (signInResponse && !signInResponse.error) {
-      //Redirect to homepage (/timeline)
-      router.push("/timeline");
-    } else {
-      console.log("Error: ", signInResponse);
-      setError("Your Email or Password is wrong!");
-    }
-  };
+		if (signInResponse && !signInResponse.error) {
+			//Redirect to homepage (/timeline)
+			router.push('/timeline');
+		} else {
+			console.log('Error: ', signInResponse);
+			setError('Your Email or Password is wrong!');
+		}
+	};
 
-  return (
-    <div
-      className="w-full mt-3 text-xl text-black font-semibold flex flex-col"
-      // onSubmit={handleSubmit}
-    >
-      {error && (
-        <span className="p-4 mb-2 text-lg font-semibold text-white bg-red-500 rounded-md">
-          {error}
-        </span>
-      )}
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        required
-        className="w-full px-4 py-4 mb-4 border border-gray-300 rounded-md"
-      />
+	return (
+		<div
+			className="w-full mt-3 text-xl text-black font-semibold flex flex-col"
+			// onSubmit={handleSubmit}
+		>
+			{error && <span className="p-4 mb-2 text-lg font-semibold text-white bg-red-500 rounded-md">{error}</span>}
+			<input type="email" name="email" placeholder="Email" required className="w-full px-4 py-4 mb-4 border border-gray-300 rounded-md" />
 
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        required
-        className="w-full px-4 py-4 mb-4 border border-gray-300 rounded-md"
-      />
-
-      <button
-        onClick={submit}
-        type="submit"
-        className="w-full h-12 px-6 mt-4 text-lg text-white transition-colors duration-150 bg-black rounded-lg focus:shadow-outline hover:bg-gray-600"
-      >
-        Create account
-      </button>
-    </div>
-  );
+			<input type="password" name="password" placeholder="Password" required className="w-full px-4 py-4 mb-4 border border-gray-300 rounded-md" />
+			<Link href="/journal">
+				<button
+					onClick={submit}
+					type="submit"
+					className="w-full h-12 px-6 mt-4 text-lg text-white transition-colors duration-150 bg-black rounded-lg focus:shadow-outline hover:bg-gray-600"
+				>
+					Create account
+				</button>
+			</Link>
+		</div>
+	);
 }
--- app/entries/page.tsx
+++ app/entries/page.tsx
@@ -1,154 +1,99 @@
-"use client";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import NavBar from "@/components/navBar";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import toast, { Toaster } from "react-hot-toast";
+'use client';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import NavBar from '@/components/navBar';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import toast, { Toaster } from 'react-hot-toast';
 
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { useState } from "react";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
+import { useState } from 'react';
 
 export default function Home() {
-  const [clicked, setClicked] = useState(false);
+	const [clicked, setClicked] = useState(false);
 
-  const save = async () => {
-    setClicked(true);
+	const save = async () => {
+		setClicked(true);
 
-    setTimeout(() => {
-      setClicked(false);
-      toast.success("Form saved successfully!");
-    }, 2000);
-  };
-  return (
-    <div className="bg-bgGradient relative">
-      <Toaster />
-      <NavBar></NavBar>
-      <div className="flex min-h-screen flex-col items-center justify-between p-28 bg-bgGradient">
-        <div className="w-full bg-gray-100 p-5 border rounded-md mt-10">
-          <Accordion type="single" collapsible className="w-full">
-            <h4 className="scroll-m-20 py-1.5 text-lg text-blue-950 font-semibold tracking-tight">
-              Your Entries
-            </h4>
-            <AccordionItem value="item-1">
-              <AccordionTrigger className="text-blue-950">
-                01.13.2024
-              </AccordionTrigger>
-              <AccordionContent>
-                <p className="leading-7 [&:not(:first-child)]:mt-6 text-blue-950">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Commodi perspiciatis explicabo beatae, unde nam, eaque
-                  suscipit tempora officia temporibus esse, laborum alias
-                  dolorem! Quos, tenetur? At delectus quia odio laudantium.
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Dolorum, culpa voluptatibus facilis doloremque ipsam
-                  praesentium ab veniam doloribus ducimus consectetur harum
-                  dolorem nisi obcaecati ipsum quibusdam, deleniti ipsa, quae
-                  beatae.
-                </p>
-                <div className="flex flex-row flex-wrap gap-x-4">
-                  <img
-                    src="https://www.psychologicalscience.org/redesign/wp-content/uploads/2023/10/Oct23-Trigger-Warnings-Featured-1024x682.jpg"
-                    className="h-48 w-52 py-4"
-                  ></img>
-                  <img
-                    src="https://www.psychologicalscience.org/redesign/wp-content/uploads/2023/10/Oct23-Trigger-Warnings-Featured-1024x682.jpg"
-                    className="h-48 w-52 py-4"
-                  ></img>
-                </div>
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-2">
-              <AccordionTrigger className="text-blue-950">
-                01.18.2024
-              </AccordionTrigger>
-              <AccordionContent>
-                <p className="leading-7 [&:not(:first-child)]:mt-6 text-blue-950">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Commodi perspiciatis explicabo beatae, unde nam, eaque
-                  suscipit tempora officia temporibus esse, laborum alias
-                  dolorem! Quos, tenetur? At delectus quia odio laudantium.
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Dolorum, culpa voluptatibus facilis doloremque ipsam
-                  praesentium ab veniam doloribus ducimus consectetur harum
-                  dolorem nisi obcaecati ipsum quibusdam, deleniti ipsa, quae
-                  beatae.
-                </p>
-                <div className="flex flex-row flex-wrap gap-x-4">
-                  <img
-                    src="https://www.psychologicalscience.org/redesign/wp-content/uploads/2023/10/Oct23-Trigger-Warnings-Featured-1024x682.jpg"
-                    className="h-48 w-52 py-4"
-                  ></img>
-                  <img
-                    src="https://www.psychologicalscience.org/redesign/wp-content/uploads/2023/10/Oct23-Trigger-Warnings-Featured-1024x682.jpg"
-                    className="h-48 w-52 py-4"
-                  ></img>
-                </div>
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-3">
-              <AccordionTrigger className="text-blue-950">
-                01.25.2024
-              </AccordionTrigger>
-              <AccordionContent>
-                <p className="leading-7 [&:not(:first-child)]:mt-6 text-blue-950">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Commodi perspiciatis explicabo beatae, unde nam, eaque
-                  suscipit tempora officia temporibus esse, laborum alias
-                  dolorem! Quos, tenetur? At delectus quia odio laudantium.
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Dolorum, culpa voluptatibus facilis doloremque ipsam
-                  praesentium ab veniam doloribus ducimus consectetur harum
-                  dolorem nisi obcaecati ipsum quibusdam, deleniti ipsa, quae
-                  beatae.
-                </p>
-                <div className="flex flex-row flex-wrap gap-x-4">
-                  <img
-                    src="https://www.psychologicalscience.org/redesign/wp-content/uploads/2023/10/Oct23-Trigger-Warnings-Featured-1024x682.jpg"
-                    className="h-48 w-52 py-4"
-                  ></img>
-                  <img
-                    src="https://www.psychologicalscience.org/redesign/wp-content/uploads/2023/10/Oct23-Trigger-Warnings-Featured-1024x682.jpg"
-                    className="h-48 w-52 py-4"
-                  ></img>
-                </div>
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-4">
-              <AccordionTrigger className="text-blue-950">
-                02.05.2024
-              </AccordionTrigger>
-              <AccordionContent>
-                <p className="leading-7 [&:not(:first-child)]:mt-6 text-blue-950">
-                  I'm in an abusive relationship and I don't know what to do.
-                </p>
-                <div className="flex flex-row flex-wrap gap-x-4">
-                  <img
-                    src="../../../images/plot.png"
-                    className="h-48 w-52 py-4"
-                  ></img>
-                </div>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-          <div className="flex justify-center py-6 text-xl">
-            <Button className="bg-blue-950" onClick={save}>
-              {clicked ? "Reporting..." : "File Report"}
-            </Button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+		setTimeout(() => {
+			setClicked(false);
+			toast.success('Form saved successfully!');
+		}, 2000);
+	};
+	return (
+		<div className="bg-bgGradient relative">
+			<Toaster />
+			<NavBar></NavBar>
+			<div className="flex min-h-screen flex-col items-center justify-between p-28 bg-bgGradient">
+				<div className="w-full bg-gray-100 p-5 border rounded-md mt-10">
+					<Accordion type="single" collapsible className="w-full">
+						<h4 className="scroll-m-20 py-1.5 text-lg text-blue-950 font-semibold tracking-tight">Your Entries</h4>
+						<AccordionItem value="item-1">
+							<AccordionTrigger className="text-blue-950">01.13.2024</AccordionTrigger>
+							<AccordionContent>
+								<p className="leading-7 [&:not(:first-child)]:mt-6 text-blue-950">
+									Right away he started telling people I was his wife. He would not allow me to go anywhere without letting him know. I once
+									lied to him to go play Scrabble with some people. He found out and got very angry. He constantly would bring up that he
+									couldn't trust me because I lied about that.
+								</p>
+								<div className="flex flex-row flex-wrap gap-x-4">
+									<img
+										src="https://www.psychologicalscience.org/redesign/wp-content/uploads/2023/10/Oct23-Trigger-Warnings-Featured-1024x682.jpg"
+										className="h-48 w-52 py-4"
+									></img>
+								</div>
+							</AccordionContent>
+						</AccordionItem>
+						<AccordionItem value="item-2">
+							<AccordionTrigger className="text-blue-950">01.18.2024</AccordionTrigger>
+							<AccordionContent>
+								<p className="leading-7 [&:not(:first-child)]:mt-6 text-blue-950">
+									You don't realise the damage you are doing by believing the lies you are told to control you and isolate you from everyone
+									who can potentially take you away. You shrug off the occasional pushing and shoving, and item throwing, slamming doors and
+									threats of violence because you're scared that if you confront them, the irrational behaviour intensifies.
+								</p>
+								<div className="flex flex-row flex-wrap gap-x-4">
+									<img
+										src="https://www.psychologicalscience.org/redesign/wp-content/uploads/2023/10/Oct23-Trigger-Warnings-Featured-1024x682.jpg"
+										className="h-48 w-52 py-4"
+									></img>
+								</div>
+							</AccordionContent>
+						</AccordionItem>
+						<AccordionItem value="item-3">
+							<AccordionTrigger className="text-blue-950">01.25.2024</AccordionTrigger>
+							<AccordionContent>
+								<p className="leading-7 [&:not(:first-child)]:mt-6 text-blue-950">
+									Then he was throwing things, slamming doors, breaking or taking my things, threatening me. In time, I would just walk away
+									from that, so he began putting his hands on me. Yanking me by the hair, shoving me, grabbing my wrists, physically putting
+									me outside and locking the door, etc.
+								</p>
+								<div className="flex flex-row flex-wrap gap-x-4">
+									<img
+										src="https://www.psychologicalscience.org/redesign/wp-content/uploads/2023/10/Oct23-Trigger-Warnings-Featured-1024x682.jpg"
+										className="h-48 w-52 py-4"
+									></img>
+								</div>
+							</AccordionContent>
+						</AccordionItem>
+						<AccordionItem value="item-4">
+							<AccordionTrigger className="text-blue-950">02.05.2024</AccordionTrigger>
+							<AccordionContent>
+								<p className="leading-7 [&:not(:first-child)]:mt-6 text-blue-950">
+									I'm in an abusive relationship and I don't know what to do.
+								</p>
+								<div className="flex flex-row flex-wrap gap-x-4">
+									<img src="https://www.psychologicalscience.org/redesign/wp-content/uploads/2023/10/Oct23-Trigger-Warnings-Featured-1024x682.jpg" className="h-48 w-52 py-4"></img>
+								</div>
+							</AccordionContent>
+						</AccordionItem>
+					</Accordion>
+					<div className="flex justify-center py-6 text-xl">
+						<Button className="bg-blue-950" onClick={save}>
+							{clicked ? 'Reporting...' : 'File Report'}
+						</Button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
