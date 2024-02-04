00c23e643fcb63f5aa0924b0ad8600fb6d9dca9b
sgnahore <stephaniergnahore@gmail.com>
2024-02-04 02:28:19 +0000

    CSS COMPLETE

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,10 +8,10 @@ import {
 // import { getServerSession } from "next-auth";
 // import { authConfig } from "@/lib/auth";
 // import { redirect } from "next/navigation";
-// import { CredentialsForm } from "@/components/credentialsForm";
 // import { getCsrfToken } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import styles from "./styles/login.css";
+import { CredentialsForm } from "../components/credentialsForm";
 
 export default async function SignInPage() {
   // const session = await getServerSession(authConfig);
@@ -21,19 +21,22 @@ export default async function SignInPage() {
   // if (session) return redirect("/timeline");
 
   return (
+   
      <main className="flex min-h-screen flex-col items-center justify-between p-24" style={{ background: "linear-gradient(180deg, rgba(0, 17, 43, 1) 18%, rgba(2, 30, 75, 0.47805059523809523) 100%)" }}>
-     
+      <div className= "flex min-h-screen flex-col items-center justify-between p-20 rounded-lg"style={{ background: "white", minHeight:"35vh" }}>
       
 
       <div className={styles.grid}>
-      <h1>Sign in</h1>
-      <h3>Enter your details to log in</h3>
+      <h1 className="text-3xl font-bold mb-4 text-center"
+      >Sign in</h1>
       <GoogleSignInButton />
       <GithubSignInButton />
-      <h1>
-        Or
+      <h1 className="text-center text-gray-600 p-4">
+        OR CONTINUE WITH
       </h1>
+      <CredentialsForm/>
       </div>
-
+ </div>
     </main>
+   
   )};
