dd1a9c03816ac674fd8f7c29a3e6db728f48732f
Aiturgan <aiturgan888@gmail.com>
2024-02-03 19:14:42 -0600

    changed the nav

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -4,10 +4,8 @@ import Link from 'next/link';
 const NavBar = () => {
 	return (
 		<div className="absolute top-9 flex flex-row text-white gap-2 items-center w-screen px-28 text-lg justify-between ">
-			{/* <h4 className="text-rose-300">SafeHaven</h4> */}
-
 			<div className="flex flex-row text-white gap-1">
-				<Link href="/" className="py-2 px-6 text-rose-300">
+				<Link href="/" className="py-2 pr-6 text-rose-300 font-bold text-2xl ">
 					SafeHaven
 				</Link>
 				<Link href="" className="border border-transparent rounded-xl  py-2 px-6 hover:border-white">
@@ -21,11 +19,11 @@ const NavBar = () => {
 				</Link>
 			</div>
 			<div className="flex flex-row text-white gap-x-3">
+				<Link href="" className="rounded-xl py-2 px-5 bg-red-500 text-white font-semibold hover:bg-red-600">
+					Emergency
+				</Link>
 				<Link href="" className="border border-transparent rounded-xl  py-2 px-6 hover:border-white">
 					Sign Out
-				</Link>
-				<Link href="" className="border rounded-xl py-2 px-6 text-white border-rose-300 hover:bg-zinc-200 hover:text-red-800 hover:border-none">
-					Emergency
 				</Link>
 			</div>
 		</div>
